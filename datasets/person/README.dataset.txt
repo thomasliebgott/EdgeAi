67d11effffb17557dfda115a5cedf512c2c4b207
thomasliebgott <72511441+thomasliebgott@users.noreply.github.com>
2023-04-24 09:35:44 +0200

    yolov5 working

---
+++ datasets/person/README.dataset.txt
@@ -0,0 +1,7 @@
+# undefined > 2022-04-28 9:53pm
+https://public.roboflow.ai/object-detection/undefined
+
+Provided by undefined
+License: CC BY 4.0
+
+undefined
--- train.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolov8n.pt")
-
-model.train(task = detect, mode=train, epochs = 2, data = "data.yaml", imgsz = 640)
